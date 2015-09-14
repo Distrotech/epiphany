92412d0aa272419879d0d8f711275812a25cd496
Michael Catanzaro <mcatanzaro@igalia.com>
2015-09-13 19:21:20 -0500

    EphyDownload: Remove unneeded virtual signals
    So we can use G_DECLARE_FINAL_TYPE

--- embed/ephy-download.h
+++ embed/ephy-download.h
@@ -55,14 +55,6 @@ struct _EphyDownload
 struct _EphyDownloadClass
 {
   GObjectClass parent_class;
-
-  void (* filename_suggested) (EphyDownload *download,
-                               char *suggested_filename);
-  void (* completed)  (EphyDownload *download);
-  void (* error)      (EphyDownload *download,
-                       gint error_code,
-                       gint error_detail,
-                       char *reason);
 };
 
 typedef enum
